2c5e54c62805fca0237983b29241eefb34af7a16
丰帅 <fengshuai@fengshuaideMacBook-Pro.local>
2017-12-20 22:19:00 +0800

    第二节

---
+++ readme.txt
@@ -0,0 +1,33 @@
+study flask
+*mac python path /System/Library/Frameworks/Python.framework/Versions
+
+*install pip
+mac
+1.download get-pip.py
+2.$sudo python get-pip.py
+
+ubuntu
+$sudo apt-get install python-dev python-pip
+
+*change pip source
+$cd ~
+$mkdir .pip
+$touch pip.conf
+$vi pip.conf
+    [global]
+    index-url=http://mirrors.aliyun.com/pypi/simple/
+    [install]
+    trusted-host=mirrors.aliyun.com
+
+*install falsk
+$pip install flask
+maybe use
+$sudo pip install falsk
+
+*install virtualenv
+$sudo apt-get install virtualenv
+
+*use virtualenv
+$mkdir myproject
+$virtualenv venv
+$. venv/bin/activate
--- study1219.py
+++ study1219.py
@@ -1,17 +1,22 @@
 # coding=utf-8
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template,redirect,make_response,abort
 from werkzeug.routing import BaseConverter
-from flask.ext.script import Manager
+from werkzeug.utils import secure_filename
 
+from os import path
+# from flask.ext.script import Manager
+
+# 正则表达式路由
+'''
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
-
-
+'''
 app1 = Flask(__name__)
 # 正则表达式路由
-app1.url_map.converters['regex'] = RegexConverter
+# app1.url_map.converters['regex'] = RegexConverter
+# flask路由
 '''
 @app.route('/')
 def hello_world():
@@ -25,6 +30,10 @@ def hello_world1(uname):
 # test case
 # with app.test_request_context():
 #     print url_for('hello_world1',uname='jimu')
+# test case
+# with app.test_request_context('/login',method='POST'):
+#     assert request.path == '/login'
+#     assert request.method == 'POST'
 
 # static/style.css
 # url_for('static', filename='style.css')
@@ -41,9 +50,8 @@ def login():
 def hello(name=None):
     return render_template('hello.html', name=name)
 '''
-
-
-# 正则表达式路由
+# 正则表达式路由,get,post响应
+'''
 @app1.route('/user/<regex("[a-z]{3}"):userid>/')
 def user(userid):
     return 'User %s' % userid
@@ -51,12 +59,31 @@ def user(userid):
 @app1.route('/login',methods=['GET', 'POST'])
 def login():
     return render_template('login.html', method=request.method)
+'''
+# 文件上传
+'''
+@app1.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f = request.files['file']
+        basepath = path.abspath(path.dirname(__file__))
+        upload_path=path.join(basepath,'static/uploads')
+        f.save(upload_path,secure_filename(f.filename))
+        return redirect(url_for('upload'))
+    return render_template('upload.html')
+'''
+# cookic获取
+#     request.cookies['aa']
+@app1.route('/')
+def index():
+    # abort(400)
+    response = make_response(render_template('index.html',title='Welcome'))
+    response.set_cookie('username','')
+    return response
 
-
-# test case
-# with app.test_request_context('/login',method='POST'):
-#     assert request.path == '/login'
-#     assert request.method == 'POST'
+@app1.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app1.run(host='0.0.0.0', debug=True)
